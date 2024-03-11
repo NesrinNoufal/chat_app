d7f79f54c1734f771daf3221f743384aa55954cc
nesrin <nesrinnoufal99@gmail.com>
2024-03-11 11:57:44 +0530

    set up user model
    	modified:   backend/controllers/auth.controller.js
    	modified:   backend/server.js
    	modified:   backend/utils/generateToken.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,16 +1,32 @@
-export const signup = async(req, res) => {
-   try{
-    const {fullName,userName,password,confirmPassword,gender} =  req.body;
-   }
-   catch(error){
-    
-   }
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+export const signup = async (req, res) => {
+	try {
+		const { fullName, username, password, confirmPassword, gender } = req.body;
+
+		
+	} catch (error) {
+		console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
-export const login = (req, res) => {
-    console.log("loginUser");
+export const login = async (req, res) => {
+	try {
+		
+	} catch (error) {
+		console.log("Error in login controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
 export const logout = (req, res) => {
-    console.log("logoutUser");
+	try {
+		
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,18 @@ import connectToMongodb from "./db/connectToMongodb.js";
 
 
 const app = express();
+const PORT =process.env.PORT || 5000;
 
 dotenv.config();
 
-const PORT =process.env.PORT || 5000;
-
-app.get("/",(req,res) => {
-    //root route http://localhost:5000/
-    res.send("Hello World!!");
-});
-
+app.use(express.json()); //to parse the incoming requests with JSON payloads(from req.body)
 app.use("/api/auth",authRoutes)
+
+
+// app.get("/",(req,res) => {
+//     //root route http://localhost:5000/
+//     res.send("Hello World!!");
+// });
 
 app.listen(5000, () => {
     connectToMongodb()

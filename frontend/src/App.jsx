b41b86ecd14fea30645f5f9f7d892f35a97c3259
nesrin <nesrinnoufal99@gmail.com>
2024-03-13 00:01:05 +0530

    home page design - sidebar searchInput component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login/login';
+import SignUp from './pages/signup/signup';
+import Home from './pages/home/home';
 
 function App() {
  
 
   return (
     <div className = 'p-4 h-screen flex items-center justify-center'>
-      <Login/>
+      <Home/>
     </div>
   );
 }
